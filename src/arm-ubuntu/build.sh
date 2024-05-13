5ea3e56211e741be7419bf7528275a3cbccaeea6
Harshil Patel <harshilp2107@gmail.com>
2024-05-13 10:08:48 -0700

    resources: Update arm image to ubuntu 22.04 (#32)
    - Update building image using packer.
    - Add scripts and exot events to the image.

---
+++ src/arm-ubuntu/build.sh
@@ -0,0 +1,13 @@
+PACKER_VERSION="1.10.0"
+
+if [ ! -f ./packer ]; then
+    wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_arm64.zip;
+    unzip packer_${PACKER_VERSION}_linux_arm64.zip;
+    rm packer_${PACKER_VERSION}_linux_arm64.zip;
+fi
+
+# Install the needed plugins
+./packer init arm-ubuntu.pkr.hcl
+
+# Build the image
+./packer build arm-ubuntu.pkr.hcl
